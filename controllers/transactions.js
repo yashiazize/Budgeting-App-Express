3cd83ddd2842be1fef83a47d1a522ee7181106ae
Yashiry Azize <azizeyashiry@gmail.com>
2021-06-15 21:33:17 -0400

    added get w/ id and post

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,8 +1,23 @@
 const transactions = require("express").Router();
 const transactionsArray = require("../models/transaction");
 
+transactions.get("/:id", (req, res) => {
+    const { id } = req.params;
+    if (transactionsArray[id]) {
+        res.json(transactionsArray[id]);
+    } else {
+        res.redirect("*")
+    }
+})
+
 transactions.get("/", (req, res) => {
     res.json(transactionsArray)
+})
+
+transactions.post("/", (req, res) => {
+    const { body } = req;
+    transactionsArray.push(req.body)
+    res.json(transactionsArray[transactionsArray.length - 1])
 })
 
 module.exports = transactions;
